7e73c3feba7dd1d399934af64cd7ace630ec18cf
Andre Lillebakk <andre@lillebakk.com>
2015-07-18 00:35:39 +0200

    lagt til print tekst

--- CppApplication_2/nbproject/Makefile-Release.mk
+++ CppApplication_2/nbproject/Makefile-Release.mk
@@ -35,6 +35,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
+	${OBJECTDIR}/_ext/1481873261/bcm2835.o \
 	${OBJECTDIR}/gpio.o
 
 
@@ -61,6 +62,11 @@ LDLIBSOPTIONS=
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cppapplication_2: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cppapplication_2 ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/_ext/1481873261/bcm2835.o: ../../../../Downloads/bcm2835-1.42/src/bcm2835.c 
+	${MKDIR} -p ${OBJECTDIR}/_ext/1481873261
+	${RM} "$@.d"
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/_ext/1481873261/bcm2835.o ../../../../Downloads/bcm2835-1.42/src/bcm2835.c
 
 ${OBJECTDIR}/gpio.o: gpio.cpp 
 	${MKDIR} -p ${OBJECTDIR}
